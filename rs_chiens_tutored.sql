b7d06527d0619aa8e3dfd0064b9cbf9d4408c4ef
alkbron <50137894+alkbron@users.noreply.github.com>
2020-02-27 22:44:38 +0100

    Add files via upload
    [export] base de données

--- rs_chiens_tutored.sql
+++ rs_chiens_tutored.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : 127.0.0.1:3306
--- Généré le :  sam. 15 fév. 2020 à 15:47
+-- Généré le :  jeu. 27 fév. 2020 à 21:43
 -- Version du serveur :  5.7.24
 -- Version de PHP :  7.2.14
 
@@ -103,11 +103,13 @@ CREATE TABLE IF NOT EXISTS `imagepost` (
 --
 
 INSERT INTO `imagepost` (`idImage`, `urlImage1`, `urlImage2`, `urlImage3`, `urlImage4`) VALUES
-(1, '../img/chiot1_1', '', '', ''),
-(2, '../img/chiot2_1', '', '', ''),
-(3, '../img/chiot3_1', '', '', ''),
-(4, '../img/chiot4_1', '', '', ''),
-(5, '../img/chiot1_2', '', '', '');
+(1, 'img/chiot1_1', '', '', ''),
+(2, 'img/chiot2_1', '', '', ''),
+(3, 'img/chiot3_1', '', '', ''),
+(4, 'img/chiot4_1', '', '', ''),
+(5, 'img/chiot1_2', '', '', ''),
+(6, 'uploads/chiot5_2.jpg', '', '', ''),
+(7, 'uploads/chiot2_0.jpg', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -130,11 +132,13 @@ CREATE TABLE IF NOT EXISTS `posts` (
 --
 
 INSERT INTO `posts` (`idPost`, `txtPost`, `idImageAssoc`, `idChiot`, `datePost`) VALUES
-(1, 'Philou se sent très bien dans sa nouvelle famille! Gros bisous de sa part :)', 1, 1, '2020-01-02'),
-(2, 'Volt se familiarise déjà avec notre chat Tesla, que de bonheur ! Nous pensons à prendre un hamster et le nommer Ampère hahaha \r\nGros bisous !  ', 2, 2, '2020-01-15'),
-(3, 'Coucou de la part de Rex et son nouveau jouet préféré ! Pouet Pouet !!! :D ', 3, 3, '2020-02-01'),
-(4, 'Le dressage de Sacha a commencé hier, il connaît déjà plein de nouveaux tours ! Sacha donne la patte ! haha ', 4, 4, '2020-02-12'),
-(5, 'Philou dort comme un lard ! hahaha bonne nuit tout le monde ! ', 5, 1, '2020-02-10');
+(1, 'Philou se sent tr&egrave;s bien dans sa nouvelle famille! Gros bisous de sa part :)', 1, 1, '2020-01-02'),
+(2, 'Volt se familiarise d&eacute;j&agrave; avec notre chat Tesla, que de bonheur ! Nous pensons &agrave; prendre un hamster et le nommer Amp&egrave;re hahaha \r\nGros bisous !  ', 2, 2, '2020-01-15'),
+(3, 'Coucou de la part de Rex et son nouveau jouet pr&eacute;f&eacute;r&eacute; ! Pouet Pouet !!! :D ', 3, 3, '2020-02-01'),
+(4, 'Le dressage de Sacha a commenc&eacute; hier, il conna&icirc;t d&eacute;j&agrave; plein de nouveaux tours ! Sacha donne la patte ! haha ', 4, 4, '2020-02-12'),
+(5, 'Philou dort comme un lard ! hahaha bonne nuit tout le monde ! ', 5, 1, '2020-02-10'),
+(6, 'BONJOUR A TOUS, fin de transmianfoznaozmd', 6, 5, '2020-02-25'),
+(7, 'OUAAAAAIS ', 7, 2, '2020-02-25');
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
